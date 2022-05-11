f64a4d2cff2a61d34236004a671951e32db70f27
Yongwook Jung <ywjung99@naver.com>
2022-05-11 14:49:08 +0900

    fix

--- lib/impasse_projects_helper_patch.rb
+++ lib/impasse_projects_helper_patch.rb
@@ -1,14 +1,19 @@
 require_dependency 'projects_helper'
 
 module ImpasseProjectsHelperPatch
-  def project_settings_tabs
-    tabs = super
-    action = {:name => 'impasse', :controller => :impasse_settings, :action => :show, :partial => 'impasse_settings/show', :label => :project_module_impasse}
+  module Patches
+    module ProjectsHelperPatch
+      def project_settings_tabs
+        tabs = super
+        action = {:name => 'impasse', :controller => :impasse_settings, :action => :show, :partial => 'impasse_settings/show', :label => :project_module_impasse}
 
-    tabs << action if User.current.allowed_to?(action, @project)
+        tabs << action if User.current.allowed_to?(action, @project)
 
-    tabs
+        tabs
+      end
+    end
   end
 end
 
-ProjectsHelper.prepend(ImpasseProjectsHelperPatch)
+# apply tab
+ProjectsController.send :helper, ImpasseProjectsHelperPatch::Patches::ProjectsHelperPatch
